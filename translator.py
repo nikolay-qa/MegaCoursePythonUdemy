b70cf5d6ce2ef38a9b38504c7a70f3de59311d88
nikolay-qa <53169960+nikolay-qa@users.noreply.github.com>
2019-09-25 22:45:39 +0300

    Alpha-1

---
+++ translator.py
@@ -0,0 +1,35 @@
+import json
+from pathlib import Path
+from difflib import get_close_matches
+
+
+data_path = Path(r"C:\CodeNoim\MegaCoursePython3\data.json")
+data = json.load(open(data_path))
+
+
+def translator(w):
+    w = w.lower()
+    if w in data:
+        return data[w]
+    elif w.title() in data:
+        return data[w.title()]
+    elif w.upper() in data:
+        return data[w.upper()]
+    elif len(get_close_matches(w, data.keys(), cutoff=0.8)) > 0:
+        user_i = input("Did you mean %s instead? Enter Y/N: " % get_close_matches(w, data.keys(), cutoff=0.8)[0])
+        if user_i == "Y":
+            return data[get_close_matches(w, data.keys(), cutoff=0.8)[0]]
+        elif user_i == "N":
+            return "The word does not exist. Double check it."
+        else:
+            return "We do not understand your entry."
+    else:
+        return "This word does not exist. Please double check it."
+
+
+word = input("Enter your word: ")
+output = translator(word)
+if type(output) == list:
+    print("Your word's meaning is:\n", *output, sep='\n')
+else:
+    print(output)
